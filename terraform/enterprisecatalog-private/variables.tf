db33efc08b1421247507ca9dcfed893e6ba085eb
bala Siva <bala@us.ibm.com>
2020-05-28 17:35:49 -0500

    Script Updated

--- terraform/enterprisecatalog-private/variables.tf
+++ terraform/enterprisecatalog-private/variables.tf
@@ -1,16 +1,18 @@
 variable "catalog_name" {
   type        = "string"
+  default     = "Austin Private Catalog"
   description = "Provide the private catalog name "
 }
 
 variable "catalog_description" {
   type        = "string"
+  default     = "Catalog description"
   description = "Provide the private catalog description"
 }
 
 variable "resource_group" {
   type        = "string"
-  default     = "default"
+  default     = "Default"
   description = "Provide the resource group"
 }
 
@@ -22,10 +24,12 @@ variable "region" {
 
 variable "apikey" {
   type        = "string"
+  default       = "apikey"
   description = "Provide the IBMCloud access APIkey"
 }
 
 variable "accessgroup_giturl" {
   type        = "string"
   description = "Provide the Access Group GITHUB URL which you want add in private catalog"
+  default       = "https://github.com/GandhiCloudLab/sandbox-iam-catalogs/blob/master/tgz/AccessGroup-AccessPolicies-0.0.1.tgz"
 }
