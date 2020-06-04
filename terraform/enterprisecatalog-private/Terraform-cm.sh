b924acb2d41d347e43796f0de12e9e684bd19425
bala Siva <bala@us.ibm.com>
2020-06-04 11:41:14 -0500

    Script Updated

--- terraform/enterprisecatalog-private/Terraform-cm.sh
+++ terraform/enterprisecatalog-private/Terraform-cm.sh
@@ -53,7 +53,9 @@ else
     echo "Catalog NAME :" ${CATALOG_NAME} ":"
      echo "Catalog DESC :" ${CATALOG_DESC} ":"
 
-    ibmcloud catalog create --name ${CATALOG_NAME} --description ${CATALOG_DESC} hide-ibm-public-catalog
+    ibmcloud catalog create --name "${CATALOG_NAME}" --description "${CATALOG_DESC}" hide-ibm-public-catalog
+
+    ibmcloud catalog create --name "$1" --description "$2" hide-ibm-public-catalog
 
     #  Add Products to Catalog
 
